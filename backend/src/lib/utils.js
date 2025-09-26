70cde87b4ac69ddb8682166d10319ed95dee0d1d
alphaMale28 <manandharsanil007@gmail.com>
2025-09-26 00:08:37 -0400

    update profile endpoint added

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -3,30 +3,22 @@ import jwt from "jsonwebtoken";
 import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
-  const jwtSecretKey = ENV.JWT_SECRET_KEY;
-  if (!jwtSecretKey) {
-    throw new Error("JWT_SECRET is not configured");
+  const { JWT_SECRET_KEY } = ENV;
+  if (!JWT_SECRET_KEY) {
+    throw new Error("JWT_SECRET_KEY is not configured");
   }
 
-  // const age = 1000 * 60 * 60 * 24 * 7;
+  const age = 1000 * 60 * 60 * 24 * 7;
 
-  const token = jwt.sign({ id: userId }, ENV.JWT_SECRET_KEY, {
+  const token = jwt.sign({ userId }, JWT_SECRET_KEY, {
     expiresIn: "7d",
   });
 
-  // res.cookies("jwt", token, {
-  //   httpOnly: true,
-  //   sameSite: "Strict",
-  //   maxAge: 1000 * 60 * 60 * 24 * 7,
-  //   secure: ENV.NODE_ENC === "development" ? false : true,
-  // });
-
   res.cookie("jwt", token, {
     httpOnly: true,
-    secure: ENV.NODE_ENV === "production", // true only on HTTPS
-    sameSite: ENV.NODE_ENV === "production" ? "none" : "lax",
-    path: "/",
-    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    sameSite: "Strict",
+    maxAge: age,
+    secure: ENV.NODE_ENV === "development" ? false : true,
   });
 
   return token;
